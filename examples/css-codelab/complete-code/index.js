328534b5359cb74cc9a66a6e6384771c71ec5420
Ronald Bourret <rbourret@google.com>
2025-12-01 17:35:52 +0000

    fix: Remove classic.css

--- examples/css-codelab/complete-code/index.js
+++ examples/css-codelab/complete-code/index.js
@@ -4,10 +4,8 @@ let workspace = null;
 
 function start() {
   // Create main workspace.
-  const nullTheme = Blockly.Theme.defineTheme('nullTheme', {});
   workspace = Blockly.inject('blocklyDiv', {
     toolbox: toolboxCategories,
-    theme: nullTheme,
     renderer: 'thrasos',
   });
 }
