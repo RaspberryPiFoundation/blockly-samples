2fcf248b62511ac87aa0ab81d5b1b323ccc03c6f
Maribeth Moffatt <maribeth.moffatt@raspberrypi.org>
2026-07-01 11:28:42 -0400

    chore: update developer-tools for ts 6 and webpack 7 (#2739)

---
+++ examples/developer-tools/src/declarations.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Allow side-effect imports of CSS, which are handled by webpack loaders.
+declare module '*.css';
